7305528411bc8aba1c497423b04d43b853f1b8e4
garvjain7 <2023pietcrgarv018@poornima.org>
2026-05-08 17:04:18 +0530

    Changing redis url

--- redis_client.py
+++ redis_client.py
@@ -1,16 +1,16 @@
 import redis.asyncio as aioredis
 import os
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()
+load_dotenv()
 
-# async_redis = aioredis.from_url(
-#     os.getenv("REDIS_URL"),
-#     decode_responses=True
-# )
-
-async_redis = aioredis.Redis(
-    host="localhost",
-    port=6379,
+async_redis = aioredis.from_url(
+    os.getenv("REDIS_URL"),
     decode_responses=True
 )
+
+# async_redis = aioredis.Redis(
+#     host="localhost",
+#     port=6379,
+#     decode_responses=True
+# )
